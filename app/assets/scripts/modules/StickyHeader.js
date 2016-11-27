c2312d3c7ee59f5f709f94501798191cf4ac4a2c
Azkom <azkom@t-online.hu>
2016-11-27 22:42:18 +0100

    commit -m 'Fault repair.'

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,41 +1,8 @@
-<<<<<<< HEAD
-=======
-
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 import smoothScroll from 'jquery-smooth-scroll';
 
-
 class StickyHeader {
-<<<<<<< HEAD
-  constructor() {
-    this.siteHeader = $(".site-header");
-    this.headerTriggerElement = $(".large-hero__title");
-    this.createHeaderWaypoint();
-    this.pageSections = $(".page-section");
-    this.headerLinks = $(".primary-nav a");
-    this.createPageSectionWaypoints();
-    this.addSmoothScrolling();
-  }
-
-  addSmoothScrolling() {
-    this.headerLinks.smoothScroll();
-    }
-
-  createHeaderWaypoint() {
-    var that = this;
-    new Waypoint({
-      element: this.headerTriggerElement[0],
-      handler: function(direction) {
-        if (direction == "down") {
-          that.siteHeader.addClass("site-header--dark");
-        } else {
-          that.siteHeader.removeClass("site-header--dark");
-        }
-      }
-    });
-  }
-
     constructor () {
         this.siteHeader = $(".site-header");
         // The next line stores the status of the class large-hero...
